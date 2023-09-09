6fedda3815a935635aedbdc35d41bb2c8fccd324
Thota Sri Sai Chandu <t.s.s.chandu444@gmail.com>
2023-09-10 01:54:08 +0530

    backend-new-update

--- server/models/doctorModel.js
+++ server/models/doctorModel.js
@@ -19,8 +19,7 @@ const doctorSchema = new mongoose.Schema(
             required: true,
         },
         website: {
-            type: String,
-            required: true,
+            type: String
         },
         address: {
             type: String,
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -9,6 +9,45 @@ const jwt = require("jsonwebtoken")
 const Appointment = require('../models/appointmentModel')
 const authMiddleware = require('../middlewares/authMiddleware')
 
+router.post("/register", async (req, res) => {
+  try {
+    const userExists = await User.findOne({ email: req.body.email });
+    if (userExists) {
+      return res.status(200).send({ message: "User already exists", success: false });
+    }
+    const password = req.body.password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    req.body.password = hashedPassword;
+    const newuser = new User(req.body);
+    await newuser.save()
+    res.status(200).send({ message: "User created successfully", success: true });
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ message: "Error creating user", success: false, error });
+  }
+})
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(200).send({ message: "User does not exist" });
+    }
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (!isMatch) {
+      return res.status(200).send({ message: "Password is incorrect" });
+    } else {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "10d"
+      });
+      res.status(200).send({ message: "Login Successful", success: true, data: token })
+    }
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({ message: "Error Logging In", success: false, error });
+  }
+})
+
 function sendEmail({ recipient_email, OTP }) {
   return new Promise((resolve, reject) => {
     var transporter = nodemailer.createTransport({
@@ -84,45 +123,6 @@ router.post("/reset-password", async (req, res) => {
   } catch (error) {
     console.log(error)
     res.status(500).send({ message: "Error updating Password", success: false, error });
-  }
-})
-
-router.post("/register", async (req, res) => {
-  try {
-    const userExists = await User.findOne({ email: req.body.email });
-    if (userExists) {
-      return res.status(200).send({ message: "User already exists", success: false });
-    }
-    const password = req.body.password
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
-    req.body.password = hashedPassword;
-    const newuser = new User(req.body);
-    await newuser.save()
-    res.status(200).send({ message: "User created successfully", success: true });
-  } catch (error) {
-    console.log(error)
-    res.status(500).send({ message: "Error creating user", success: false, error });
-  }
-})
-router.post("/login", async (req, res) => {
-  try {
-    const user = await User.findOne({ email: req.body.email });
-    if (!user) {
-      return res.status(200).send({ message: "User does not exist" });
-    }
-    const isMatch = await bcrypt.compare(req.body.password, user.password);
-    if (!isMatch) {
-      return res.status(200).send({ message: "Password is incorrect" });
-    } else {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-        expiresIn: "10d"
-      });
-      res.status(200).send({ message: "Login Successful", success: true, data: token })
-    }
-  } catch (error) {
-    console.log(error)
-    res.status(500).send({ message: "Error Logging In", success: false, error });
   }
 })
 
